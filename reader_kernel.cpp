b2da6e0746053cc79c6c7cca2f96a03d99acc4f2
vdmrzv <vdmrzv@proton.me>
2025-07-22 20:25:40 +0300

    code clean up in reader kernel

--- reader_kernel.cpp
+++ reader_kernel.cpp
@@ -11,26 +11,18 @@ inline uint32_t calc_src_tile_index(
     size_t remaining = dst_tile_id;
     uint32_t src_multi_dim[rank];
     uint32_t dst_multi_dim[rank];
-    uint32_t num_dims_to_flip = 2; // TODO fix this
 
     // 1. Convert output tile linear index to multi-dimensional index
-    for (uint32_t i = 0; i < rank; ++i) {  
+    for (uint32_t i = 0; i < rank; ++i) {
         uint32_t dim = rank - 1 - i;
         dst_multi_dim[dim] = remaining % tiled_shape[dim];
         remaining /= tiled_shape[dim];
     }
 
+
     // 2. Based on 1) compute multi-dimensional index for the source tile
     for (uint32_t i = 0; i < rank; ++i) {
-        bool should_flip = false;
-        for (uint32_t j = 0; j < num_dims_to_flip; j++) {
-            if (dims_to_flip[j] == i) {
-                should_flip = true;
-                break;
-            }
-        }
-
-        if (should_flip) {
+        if (dims_to_flip[i]) {
             src_multi_dim[i] = tiled_shape[i] - dst_multi_dim[i] - 1;
         } else {
             src_multi_dim[i] = dst_multi_dim[i];
@@ -67,6 +59,7 @@ void kernel_main() {
     constexpr uint32_t FACE_HEIGHT = get_compile_time_arg_val(5);
     constexpr uint32_t FACE_WIDTH = get_compile_time_arg_val(6);
 
+
     // ------------------------------------------------------------------------
     // 2) Runtime arguments
     // ------------------------------------------------------------------------
@@ -74,10 +67,11 @@ void kernel_main() {
     const uint32_t start_tile = get_arg_val<uint32_t>(1);
     const uint32_t end_tile  = get_arg_val<uint32_t>(2);
 
-    uint32_t tiled_shape[RANK], tile_strides[RANK];
+    uint32_t tiled_shape[RANK], tile_strides[RANK], dims_to_flip[RANK];
     for (uint32_t i = 0; i < RANK; i++) {
         tiled_shape[i] = get_arg_val<uint32_t>(i + 3);
         tile_strides[i] = get_arg_val<uint32_t>(i + RANK + 3);
+        dims_to_flip[i] = get_arg_val<uint32_t>(i + RANK + RANK + 3);
     }
 
     // ------------------------------------------------------------------------
@@ -89,7 +83,8 @@ void kernel_main() {
     constexpr uint32_t NUM_FACES_W = TILE_WIDTH / FACE_WIDTH;
     constexpr uint32_t NUM_FACES = NUM_FACES_H * NUM_FACES_W;
     constexpr uint32_t SUBTILE_LINE_BYTES = FACE_WIDTH * element_size;
-    constexpr uint32_t FACE_H_STRIDE_BYTES = NUM_FACES_W * FACE_HW_BYTES;
+    constexpr bool is_vertical_flip = dims_to_flip[rank - 2];
+    constexpr bool is_horizontal_flip = dims_to_flip[rank - 1];
 
     constexpr uint32_t onetile = 1;
     constexpr uint32_t cb_id = tt::CBIndex::c_0;
@@ -101,35 +96,40 @@ void kernel_main() {
         .data_format = data_format
     };
 
-    uint32_t dims_to_flip[2] = {2, 3}; // TODO fix this
-
     for (uint32_t tile_id = start_tile; tile_id < end_tile; ++tile_id) {
         cb_reserve_back(cb_id, onetile);
         uint32_t l1_buf_addr = get_write_ptr(cb_id);
-        uint32_t l1_buf_base_addr = l1_buf_addr; // save base address for debug print
- 
+        uint32_t save_addr = l1_buf_addr; // save base address for debug print
         uint32_t src_tile_id = calc_src_tile_index(
             tile_id, RANK, dims_to_flip, tiled_shape, tile_strides);
         // DPRINT << tile_id << " <- " << src_tile_id << ENDL();
- 
         uint64_t tile_base_addr = get_noc_addr(src_tile_id, s0, 0);
 
         // read faces in reverse order
         for (int32_t face = NUM_FACES - 1; face >= 0; face--) {
             uint64_t face_addr = tile_base_addr + face * FACE_HW_BYTES;
 
-            // read rows in reverse order
-            for (int32_t face_row = FACE_HEIGHT - 1; face_row >= 0; face_row--) {
+            // for (int32_t face_row = FACE_HEIGHT - 1; face_row >= 0; face_row--) {
+            // for (int32_t face_row = 0; face_row < FACE_HEIGHT; face_row++) {
+
+            // if vertical flip read rows in reverse order else read in normal order
+            int32_t step = flip ? -1 : 1;
+            int32_t start = flip ? FACE_HEIGHT - 1 : 0;
+            int32_t end = flip ? -1 : FACE_HEIGHT;
+            for (int32_t face_row = start; face_row != end; face_row += step) {
                 uint64_t face_row_addr = face_addr + face_row * SUBTILE_LINE_BYTES;
+                // TODO instead of reading line by line, read the whole tile
                 noc_async_read(face_row_addr, l1_buf_addr, SUBTILE_LINE_BYTES);
                 noc_async_read_barrier();
 
-                // Flip elements within the row in L1
-                uint32_t* row_data = reinterpret_cast<uint32_t*>(l1_buf_addr);
-                for (uint32_t i = 0; i < FACE_WIDTH / 2; ++i) {
-                    uint32_t temp = row_data[i];
-                    row_data[i] = row_data[FACE_WIDTH - 1 - i];
-                    row_data[FACE_WIDTH - 1 - i] = temp;
+                if (is_horizontal_flip) {
+                    // flip elements within the row
+                    uint32_t* row_data = reinterpret_cast<uint32_t*>(l1_buf_addr);
+                    for (uint32_t i = 0; i < FACE_WIDTH / 2; ++i) {
+                        uint32_t temp = row_data[i];
+                        row_data[i] = row_data[FACE_WIDTH - 1 - i];
+                        row_data[FACE_WIDTH - 1 - i] = temp;
+                    }
                 }
                 l1_buf_addr += SUBTILE_LINE_BYTES;
             }
@@ -139,9 +139,9 @@ void kernel_main() {
         // for (uint32_t face = 0; face < 4; face++) {
         //     for (uint32_t face_row = 0; face_row < FACE_HEIGHT; face_row++) {
         //         for (uint32_t face_col = 0; face_col < FACE_WIDTH; ++face_col) {
-        //             DPRINT << uint32_t(reinterpret_cast<uint32_t*>(l1_buf_base_addr)[face_col]) << ", ";
+        //             DPRINT << uint32_t(reinterpret_cast<uint32_t*>(save_addr)[face_col]) << ", ";
         //         }
-        //         l1_buf_base_addr += SUBTILE_LINE_BYTES;
+        //         save_addr += SUBTILE_LINE_BYTES;
         //         DPRINT << ENDL();
         //     }
         //     DPRINT << ENDL();
